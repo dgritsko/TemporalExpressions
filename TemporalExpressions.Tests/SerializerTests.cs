23ed06db9dfb95dbbd4aa3070d835fe034a80e9b
Dan Gritsko <dgritsko@gmail.com>
2017-08-27 21:21:09 -0400

    Continued work on Serializer

--- TemporalExpressions.Tests/SerializerTests.cs
+++ TemporalExpressions.Tests/SerializerTests.cs
@@ -6,7 +6,23 @@ namespace TemporalExpressions.Tests
     [TestFixture]
     public class SerializerTests
     {
+        [TestCase("{dayinmonth(count:1,day:sunday)}")]
+        [TestCase("{dayinmonth(count:5,day:monday)}")]
+        [TestCase("{dayinmonth(count:-1,day:tuesday)}")]
+        [TestCase("{dayinmonth(count:2,day:wednesday)}")]
+        [TestCase("{dayinmonth(count:1,day:thursday)}")]
+        [TestCase("{dayinmonth(count:1,day:friday)}")]
+        [TestCase("{dayinmonth(count:1,day:saturday)}")]
         [TestCase("{rangeeachyear(month:1)}")]
+        [TestCase("{rangeeachyear(startmonth:1,endmonth:2)}")]
+        [TestCase("{rangeeachyear(startmonth:1,endmonth:2,startday:3,endday:4)}")]
+        [TestCase("{difference(included:{rangeeachyear(month:1)},excluded:{rangeeachyear(month:2)})}")]
+        [TestCase("{intersection(elements:{rangeeachyear(month:1)};{rangeeachyear(month:1)})}")]
+        [TestCase("{union(elements:{rangeeachyear(month:1)};{rangeeachyear(month:1)})}")]
+        [TestCase("{regularinterval(year:2017,month:1,day:1,count:2,unit:day)}", "01/01/17")]
+        [TestCase("{true}")]
+        [TestCase("{false}")]
+        [TestCase("{not(expression:{true})}")]
         public void ShouldSerializeCorrectly(string input)
         {
             var compiled = Compiler.Compiler.Compile(input);
--- TemporalExpressions/Serializer/Serializer.cs
+++ TemporalExpressions/Serializer/Serializer.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using TemporalExpressions.Compiler.Identifiers;
+using System.Text;
+using TemporalExpressions.Compiler.Util;
 
 namespace TemporalExpressions.Serializer
 {
     public static class Serializer
     {
-        public static Dictionary<Type, Func<TemporalExpression, string>> Serializers = new Dictionary<Type, Func<TemporalExpression, string>>
+        public static Dictionary<Type, Action<TemporalExpression, StringBuilder>> Serializers = new Dictionary<Type, Action<TemporalExpression, StringBuilder>>
         {
-            { typeof(RangeEachYear), SerializeRangeEachYear }
+            { typeof(RangeEachYear), SerializeRangeEachYear },
+            { typeof(True), SerializeTrue },
+            { typeof(False), SerializeFalse },
+            { typeof(Not), SerializeNot },
         };
 
         public static string Serialize(TemporalExpression expression)
@@ -20,10 +24,14 @@ namespace TemporalExpressions.Serializer
                 throw new ArgumentException($"Unsupported type: {key}");
             }
 
-            return Serializers[key](expression);
+            var output = new StringBuilder();
+
+            Serializers[key](expression, output);
+
+            return output.ToString();
         }
 
-        public static string SerializeRangeEachYear(TemporalExpression temporalExpression)
+        public static void SerializeRangeEachYear(TemporalExpression temporalExpression, StringBuilder output)
         {
             var expression = temporalExpression as RangeEachYear;
 
@@ -31,20 +39,90 @@ namespace TemporalExpressions.Serializer
             {
                 var argument = BuildArgument(Compiler.Identifiers.RangeEachYear.Month, expression.StartMonth.ToString());
 
-                return BuildExpression(Compiler.Identifiers.Expressions.RangeEachYear, argument);
+                BuildExpression(Compiler.Identifiers.Expressions.RangeEachYear, argument, output);
             }
+            else if (expression.StartDay == 0 && expression.EndDay == 0)
+            {
+                var arguments = new List<string>()
+                {
+                    BuildArgument(Compiler.Identifiers.RangeEachYear.StartMonth, expression.StartMonth.ToString()),
+                    BuildArgument(Compiler.Identifiers.RangeEachYear.EndMonth, expression.EndMonth.ToString()),
+                };
 
-            throw new NotImplementedException();
+                BuildExpression(Compiler.Identifiers.Expressions.RangeEachYear, BuildArgumentList(arguments), output);
+            }
+            else
+            {
+                var arguments = new List<string>()
+                {
+                    BuildArgument(Compiler.Identifiers.RangeEachYear.StartMonth, expression.StartMonth.ToString()),
+                    BuildArgument(Compiler.Identifiers.RangeEachYear.EndMonth, expression.EndMonth.ToString()),
+                    BuildArgument(Compiler.Identifiers.RangeEachYear.StartDay, expression.StartDay.ToString()),
+                    BuildArgument(Compiler.Identifiers.RangeEachYear.EndDay, expression.EndDay.ToString()),
+                };
+
+                BuildExpression(Compiler.Identifiers.Expressions.RangeEachYear, BuildArgumentList(arguments), output);
+            }
         }
 
-        public static string BuildExpression(string identifier, string arguments)
+        public static void SerializeTrue(TemporalExpression temporalExpression, StringBuilder output)
         {
-            return $"{{{identifier}({arguments})}}";
+            BuildExpression(Compiler.Identifiers.Expressions.True, string.Empty, output);
+        }
+
+        public static void SerializeFalse(TemporalExpression temporalExpression, StringBuilder output)
+        {
+            BuildExpression(Compiler.Identifiers.Expressions.False, string.Empty, output);
+        }
+
+        public static void SerializeNot(TemporalExpression temporalExpression, StringBuilder output)
+        {
+            var notExpression = temporalExpression as Not;
+
+            if (notExpression != null)
+            {
+                var expression = Serialize(notExpression.Expression);
+
+                var argument = BuildArgument(Compiler.Identifiers.Not.Expression, expression);
+
+                BuildExpression(Compiler.Identifiers.Expressions.Not, argument, output);
+            }            
+        }
+
+        public static void BuildExpression(string identifier, string arguments, StringBuilder builder)
+        {
+            builder.Append(GrammarUtil.ExprStart);
+
+            builder.Append(identifier);
+
+            if (arguments.Length > 0)
+            {
+                builder.Append(GrammarUtil.ArgumentsStart);
+
+                builder.Append(arguments);
+
+                builder.Append(GrammarUtil.ArgumentsEnd);
+            }
+
+            builder.Append(GrammarUtil.ExprEnd);
         }
 
         public static string BuildArgument(string identifier, string value)
         {
-            return $"{identifier}:{value}";
+            var builder = new StringBuilder();
+
+            builder.Append(identifier);
+
+            builder.Append(GrammarUtil.IdentifierSeparator);
+
+            builder.Append(value);
+
+            return builder.ToString();
+        }
+
+        public static string BuildArgumentList(List<string> arguments)
+        {
+            return string.Join(GrammarUtil.ArgumentDelimiter.ToString(), arguments);
         }
     }
 }
